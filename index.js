28678672b96413ee29464afb57e2619da4623461
Robert Han <robmj.han@gmail.com>
2018-12-03 05:23:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase();
 }
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase();
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase())
+  console.log(string.toUpperCase());
 }
 
 function logWhisper(string) {
-  console.log(string.toLowerCase())
+  console.log(string.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-  if (string===string.toLowerCase()) 
-return ("I can't hear you!") 
+  if (string===string.toLowerCase())
+  return ("I can't hear you!");
 }
 
 function sayHiToGrandma(string) {
   if (string===string.toUpperCase())
-return ("YES INDEED!")
+return ("YES INDEED!");
 }
 
 function sayHiToGrandma(string) {
-  if (string===string("I love you, Grandma"))
+  if (string===("I love you, Grandma.")) {
+  return ("I love you, too."); }
+}
+
+
+
